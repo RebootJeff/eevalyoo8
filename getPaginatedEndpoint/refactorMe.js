d1ab6679b33f09e8bdaf942fe05631ec970900dd
Jeffrey Lee <jlee@zignallabs.com>
2017-07-10 13:52:12 -0700

    Add answers

--- getPaginatedEndpoint/refactorMe.js
+++ getPaginatedEndpoint/refactorMe.js
@@ -5,10 +5,13 @@ var _ = require('lodash');
 // Notification model uses https://github.com/edwardhotchkiss/mongoose-paginate
 
 NotificationsController.prototype.getUserNotifications = function(req, res, next) {
+  // * Perhaps this code should be wrapped in a `Promise.try` to help catch errors
   var offset = req.query.offset;
   var limit = req.query.limit;
   var userId = new ObjectId(req.param('userId'));
 
+  // * All this query building code + query modification code that comes a bit
+  // later could be bundled into a function for easier readability.
   var userQuery = {
     recipientType: 'user'
   };
@@ -29,17 +32,21 @@ NotificationsController.prototype.getUserNotifications = function(req, res, next
 
   return User.findById(userId)
   .then(function(user) {
-    if (!user) {
-      throw new NotFoundError('No such User: ' + userId);
-    }
+    // * We should throw an error if the user isn't found
 
     notificationsReadAt = user.notifications_read_at;
+    // * We can avoid relying on mutation of the query objects if we just
+    // define them when they're needed rather than before they're needed.
     userQuery.recipients = {$in: [user._id]};
     subGroupQuery.recipients = {$in: [user.subGroup._id]};
+    // * Company and User queries could be done in parallel instead of sequentially
+    // (by using `Promise.all`)
     return Company.find({'employees.$id': user._id});
   })
-  .then(function(campaigns) {
+  .then(function(companies) {
     companyQuery.recipients = {$in: _.map(companies, '_id')};
+    // * The empty object given to `#paginate` is questionable.
+    // It should be the `query` variable.
     return Notification.paginate({}, {
       offset: offset,
       limit: limit,
@@ -59,6 +66,8 @@ NotificationsController.prototype.getUserNotifications = function(req, res, next
       return Notification.count(unreadQuery);
     }
 
+    // * No need for this query. Just use the `total` count returned by the
+    // `#paginate` query.
     return Notification.count(query);
   })
   .then(function(unread){
@@ -66,6 +75,7 @@ NotificationsController.prototype.getUserNotifications = function(req, res, next
     this.addHeaders(res);
     res.send(response);
   }, function(err) {
+    // * Bad practice to use error callback within `#then`. Just use `#catch` instead.
     console.error('err');
   });
 }
--- sumObjects/writeCodeHere.js
+++ sumObjects/writeCodeHere.js
@@ -1,20 +1,24 @@
-var expect = require('chai').expect
+var _ = require('lodash');
 
-// Feel free to bring in any npm packages you want.
-
-
-function sumObjects(objs) {
-  // Your solution goes here...
+function sumObjects(arr) {
+  return _.reduce(arr, sum2Objects, {});
 }
 
+function sum2Objects(obj1, obj2) {
+  var keys1 = _.keys(obj1);
+  var keys2 = _.keys(obj2);
+  var allKeys = _.uniq(keys1.concat(keys2));
 
-// Run tests via command line: `npm test`
+  return _.reduce(allKeys, function(acc, key) {
+    var val1 = obj1[key] || 0;
+    var val2 = obj2[key] || 0;
+    acc[key] = val1 + val2;
+    return acc;
+  }, {});
+}
 
-describe('sumObjects', function() {
-
-  // Your tests go here...
-  it('should be tested', function() {
-    expect(true).to.equal(false);
-  });
-
-});
+console.log(sumObjects([
+  { a: 1, b: 2 },
+  { a: 1, b: 2 },
+  { b: 2, c: 3 }
+]));
